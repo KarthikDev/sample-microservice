91a17b19df95e7a6b0d62a17f160e7a76cede826
KarthikDev <clickforkarthik@gmail.com>
2020-08-25 19:59:55 +0200

    updated package declaration

--- src/main/java/com/learn/java/samplemicroservice/controller/GreetingController.java
+++ src/main/java/com/learn/java/samplemicroservice/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.learn.microservices.springbootmicroserviceazure.controller;
+package com.learn.java.samplemicroservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
