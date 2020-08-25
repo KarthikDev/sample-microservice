af91325a6e7c8c819476dd4f0079bc8df3dd3c6d
KarthikDev <clickforkarthik@gmail.com>
2020-08-25 19:42:18 +0200

    initial commit

---
+++ src/test/java/com/learn/java/samplemicroservice/SampleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java.samplemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
